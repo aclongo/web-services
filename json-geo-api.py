8469035f2e8cf9f780cf6f136b180e49e7f90fb2
aclongo <125140476+aclongo@users.noreply.github.com>
2023-04-05 10:22:38 -0400

    Add test and assignment locations

--- json-geo-api.py
+++ json-geo-api.py
@@ -2,6 +2,8 @@
 # contact a web service and retrieve JSON for the web service and parse that data, 
 # and retrieve the first place_id from the JSON. 
 # A place ID is a textual identifier that uniquely identifies a place as within Google Maps.
+# Test Location: "South Federal University", place_id "ChIJNeHD4p-540AR2Q0_ZjwmKJ8"
+# Assignment Location: "Tallinn University of Technology", place_id ???
 
 import urllib.request, urllib.parse, urllib.error
 import json
